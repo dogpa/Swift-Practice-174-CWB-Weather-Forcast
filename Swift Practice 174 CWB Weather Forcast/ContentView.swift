7fc6f0d82452ec1ee24973fe1b8768e0835750ff
Dogpa <dogpa0130@gmail.com>
2022-09-09 12:13:03 +0800

    Initial Commit

---
+++ Swift Practice 174 CWB Weather Forcast/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Practice 174 CWB Weather Forcast
+//
+//  Created by Dogpa's MBAir M1 on 2022/9/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Practice 174 CWB Weather Forcast/Swift_Practice_174_CWB_Weather_ForcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_174_CWB_Weather_ForcastApp.swift
+//  Swift Practice 174 CWB Weather Forcast
+//
+//  Created by Dogpa's MBAir M1 on 2022/9/9.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_174_CWB_Weather_ForcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
